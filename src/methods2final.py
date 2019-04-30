2026879cd527e05b82dca1e184de6899269b71ce
explorerwjy <explorerwjy@gmail.com>
2019-04-30 15:42:02 -0400

    init

---
+++ src/methods2final.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+import csv
+import numpy as np
+import sklearn
+import gzip
+from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
