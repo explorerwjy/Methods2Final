d1e2dc517de035aa5ec6a1f082d6da82e7052379
explorerwjy <explorerwjy@gmail.com>
2019-05-01 14:57:05 -0400

    add ref

--- src/methods2final.py
+++ src/methods2final.py
@@ -6,4 +6,10 @@ import gzip
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
+import scprep
+import scanpy as sc
 
+
+mk_genes = ["PTPRC", "PAX6", "PDGRFA", "NEUROD2", "GAD1", "AQP4"]
+major_cell_types = ["Microglia", "NPCs", "OPCs", "Excitatory_neurons", "Interneurons", "Astrocytes"]
+gene2celltype = dict(zip(mk_genes, major_cell_types))
